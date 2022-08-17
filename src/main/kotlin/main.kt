f7d0c22d04f9ab47ef58e09fbd68f4beb5998947
Evgeniy7891 <john2331832@gmail.com>
2022-08-17 12:30:13 +0500

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,43 @@
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+    val token = "c4Z2vc_5QDmGh6qIaLux0Q:APA91bGHjOzTGzudPRo5FHRF1draFSz6luXAhVSzHukvtqvm7Oxw8-v_1kBLb4KXnKOFNibENNg_9hPm5r2YxRVhuavyz2V9hqNonkp27ZrdzUABAlfWfzD6Nr2_Eq1_0A5bsXlF-FiK"
+
+    val message = Message.builder()
+        .putData("action", "LIKE")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+
+    val messagePost = Message.builder()
+        .putData("action", "POST")
+        .putData("content", """{
+          "userId": 2,
+          "userName": "Aleksandr",
+          "postId": "ривет всем!",
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+       FirebaseMessaging.getInstance().send(messagePost)
+
+}
